384bfd3be34339ce1103748dbc26453f74ad7a88
Johan Hardy <johan.hardy@spacebel.be>
2015-09-20 13:08:08 +0200

    Added the hello world example
    Signed-off-by: Johan Hardy <johan.hardy@spacebel.be>

---
+++ HelloWorld/src/HelloWorld/HelloWorld.c
@@ -0,0 +1,30 @@
+/**********************************************************************************
+ * Copyright: 2015 All rights reserved.
+ * Project: model
+ * File: HelloWorld/HelloWorld.c
+ * Code Management Tool File Version: 1.0.0
+ * Date: $Date$
+ * SDD Component: HelloWorld
+ * Language: C
+ * Author: jhy
+ * Last Change: $Id$
+ **********************************************************************************/
+
+/*!
+ * \file HelloWorld/HelloWorld.c
+ * \brief HelloWorld
+ */
+
+/* Dependency with own header */
+#include "HelloWorld/HelloWorld.h"
+
+
+/**********************************************************************************
+ * HelloWorld/HelloWorld.c
+ **********************************************************************************/
+
+/*## package HelloWorld */
+
+/**********************************************************************************
+ * File Path: HelloWorld/HelloWorld.c
+ **********************************************************************************/
---
+++ HelloWorld/src/HelloWorld/main.h
@@ -0,0 +1,76 @@
+/**********************************************************************************
+ * Copyright: 2015 All rights reserved.
+ * Project: model
+ * File: HelloWorld/main.h
+ * Code Management Tool File Version: 1.0.0
+ * Date: $Date$
+ * SDD Component: main
+ * Language: C
+ * Author: jhy
+ * Last Change: $Id$
+ **********************************************************************************/
+
+/*!
+ * \file HelloWorld/main.h
+ * \brief main
+ */
+
+#ifndef HelloWorld_main_H
+#define HelloWorld_main_H
+
+/* Dependency with parent package */
+#include "HelloWorld/HelloWorld.h"
+
+
+/**********************************************************************************
+ * HelloWorld/main.h
+ **********************************************************************************/
+
+/*## package HelloWorld */
+
+/*! 
+ * \package main
+ * \brief This object is the main module of the application.
+ *
+ * <b>Requirements traceability</b>
+ *    - SPEC-REQ-001
+ */
+
+/*## class TopLevel::main */
+
+
+
+
+
+
+
+
+
+
+
+/***************************************** FUNCTION *****************************************/
+/*!
+ * \brief This is the entry point of my application.
+ *        
+ * \param argc [in]
+ *        The number of arguments in argv.
+ *        
+ * \param argv [in]
+ *        The arguments.
+ *        
+ * \return int : The error code.
+ *        
+ * <b>Requirements traceability</b>
+ *      - SPEC-REQ-001
+ *
+ * <b>Function detailed processing extracted from source code</b>
+ *
+ ********************************************************************************************/
+/*## operation main(char argc, char ** argv) */
+int main(char argc, char ** argv);
+
+#endif
+
+/**********************************************************************************
+ * File Path: HelloWorld/main.h
+ **********************************************************************************/
